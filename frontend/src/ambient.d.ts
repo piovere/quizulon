6d528ca5c665c130da716f275f96af59de4121fa
J.R. Powers-Luhn <floobyt@gmail.com>
2025-03-14 03:55:21 -0700

    types

--- frontend/src/ambient.d.ts
+++ frontend/src/ambient.d.ts
@@ -1,24 +1,35 @@
 type Choice = {
     'id': number,
     'text': string,
-    'correct': boolean
+    'correct': boolean,
+    message_type: string
 };
 
 type Question = {
     id: number,
     text: string,
-    choices: Choice[]
+    choices: Choice[],
+    message_type: string
 };
 
 type Response = {
     question_id: number,
     answer_id: number,
-    learner_id: string
+    learner_id: string,
+    message_type: string
 };
 
 type Learner = {
     learner_id: string,
-    learner_name: string
+    learner_name: string,
+    message_type: string
 };
 
-type Message = Choice | Question | Response | Learner;
+type Data = {
+    questions: Question[],
+    learners: Learner[],
+    responses: Response[]
+    message_type: null
+}
+
+type Message = Choice | Question | Response | Learner | Data;
