64cb55fddfa075044763a7e0913b6d586bd00fbe
J.R. Powers-Luhn <floobyt@gmail.com>
2025-03-14 01:52:27 -0700

    dedicated question display component

--- frontend/src/routes/proctor/DisplayQuestion.svelte
+++ frontend/src/routes/proctor/DisplayQuestion.svelte
@@ -4,22 +4,39 @@
 
 <div>
     <div class="question">{@html question.text}</div>
+    <div class="choicebox">
     {#each question.choices as choice}
-    <div class="choice">{choice.text}</div>
+    <button class="choice">{choice.text}</button>
     {/each}
+    </div>
 </div>
 
 <style>
     div.question {
         border-radius: 1rem;
+        border-width: 3px;
         padding: 10px;
         margin: 5px;
         outline: 1px solid;
+        width: 90%;
+        justify-content: space-evenly;
+        font-size: x-large;
     }
-    div.choice {
+    button.choice {
         border-radius: 1rem;
         padding: 10px;
         margin: 5px;
         outline: 1px solid;
+        width: 45%;
+        font-size: large;
+    }
+    button.choice:hover {
+        background-color: rgb(0 0 0 / 0.4);
+    }
+    div.choicebox {
+        display: flex;
+        flex-direction: row;
+        width: 90%;
+        justify-content: space-evenly;
     }
 </style>
