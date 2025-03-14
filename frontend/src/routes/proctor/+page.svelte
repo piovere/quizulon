b6b502eec572ebde37b7bf1d20590765161e7c38
J.R. Powers-Luhn <floobyt@gmail.com>
2025-03-14 03:57:00 -0700

    message types

--- frontend/src/routes/proctor/+page.svelte
+++ frontend/src/routes/proctor/+page.svelte
@@ -4,39 +4,52 @@
     import QrCode from 'svelte-qrcode';
     import type { PageData } from './$types';
 
-    import DisplayQuestion from './DisplayQuestion.svelte';
+    import DisplayQuestion from '../DisplayQuestion.svelte';
 
     var currentQuestionId: number = $state(1);
     let learnerURL: string = $state('');
     var messages: Message[] = $state([]);
+    var questions: Question[] = $state([]);
+    var learners: Learner[] = $state([]);
+    var data: Data = $state();
 
-    let time: number = 6 * 1000;
+    let time: number = 60 * 1000;
 
     let dumbq: Question = {
+        "message_type": "Question",
         "id": 42,
         "text": "<p>I hate this stuff</p><p>Don't you?</p>",
         "choices": [
             {
                 "id": 99,
                 "text": "Reassurance only",
-                "correct": false
+                "correct": false,
+                "message_type": "Choice"
             },
             {
                 "id": 101,
                 "text": "Dalmations",
-                "correct": true
+                "correct": true,
+                "message_type": "Choice"
             }
         ]
     }
 
-    var currentQuestion = $derived(messages.length === 0 ? dumbq : messages[messages.length - 1]);
+    var currentQuestion = $derived(questions.length === 0 ? dumbq : questions[questions.length - 1]);
 
-    let ws: WebSocket = new WebSocket(`ws:localhost:8000/ws/proctor/`);
+    let ws: WebSocket = new WebSocket(`ws://localhost:8000/ws/proctor/`);
 
     ws.onmessage = function(event) {
-        let newQuestion: Question = JSON.parse(event.data) as Question;
-        console.log(JSON.parse(event.data));
-        messages.push(newQuestion);
+        let msg: Message = JSON.parse(event.data);
+        switch (msg.message_type) {
+            case "Question":
+                questions.push(msg as Question);
+                break;
+            case "Learner":
+                learners.push(msg as Learner);
+            case "Data":
+                data = msg as Data;
+        }
         setTimeout(sendMessage, time);
     }
 
@@ -61,9 +74,17 @@
         <QrCode value={learnerURL} />
         <p>Link to <a href={learnerURL}>quiz page</a></p>
     </div>
+    <div>
+        <ul>
+        {#each learners as learner}
+        <li>{learner.learner_name}</li>
+        {/each}
+        </ul>
+    </div>
     <button onclick={sendMessage}>Start the quiz!</button>
     {:else}
     {@const q = (currentQuestion as Question)}
     <DisplayQuestion question={q} {time} />
     {/if}
+    <div>{data}</div>
 </main>
